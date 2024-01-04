937b5b04fc5d0250856abe3e7b4f551b32abad8b
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2024-01-04 17:06:22 +0400

    feat: add deployment script

---
+++ solidity/scripts/Deploy.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Unlock} from '../contracts/Unlock.sol';
+import {Script, console} from 'forge-std/Script.sol';
+import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol';
+
+contract Deploy is Script {
+  Unlock public unlock;
+
+  IERC20 public constant VESTING_TOKEN = IERC20(0xFE67A4450907459c3e1FFf623aA927dD4e28c67a);
+  uint256 public constant START_TIME = 1_625_097_600;
+  uint256 public constant TOTAL_AMOUNT = 24_960_000 ether;
+  address public constant OWNER = 0x555B1Ea88dD9B9DA96bc0E35805e1D1C6802552f;
+
+  function run() public {
+    address deployer = vm.rememberKey(vm.envUint('DEPLOYER_PRIVATE_KEY'));
+
+    require(START_TIME > 0, 'START_TIME');
+    require(TOTAL_AMOUNT > 0, 'TOTAL_AMOUNT');
+    require(OWNER != address(0), 'OWNER');
+    require(address(VESTING_TOKEN) != address(0), 'VESTING_TOKEN');
+
+    vm.startBroadcast(deployer);
+    unlock = new Unlock(START_TIME, OWNER, VESTING_TOKEN, TOTAL_AMOUNT);
+    vm.stopBroadcast();
+
+    require(unlock.owner() == OWNER, 'owner');
+    require(unlock.START_TIME() == START_TIME, 'START_TIME');
+    require(unlock.TOTAL_AMOUNT() == TOTAL_AMOUNT, 'TOTAL_AMOUNT');
+    require(unlock.VESTING_TOKEN() == VESTING_TOKEN, 'VESTING_TOKEN');
+  }
+}
