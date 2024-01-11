3f4fb183a4d6bed4ffa5dfee66a09aefd9bbcf40
Dristpunk <dristpunk@gmail.com>
2024-01-11 19:05:28 +0300

    fix: change dates and asserts

--- solidity/test/integration/LlamaVesting.t.sol
+++ solidity/test/integration/LlamaVesting.t.sol
@@ -3,14 +3,12 @@ pragma solidity 0.8.20;
 
 import {IntegrationBase} from 'test/integration/IntegrationBase.sol';
 
-import {console} from 'forge-std/console.sol';
 /**
  * TODO: Add generic test to show the behaviour of vesting contract + llamaPay stream
  *       At the beginning of the unlocking period: 0 tokens
  *       Then limited by vesting contract
  *       Then limited by llamaPay (after vesting period has ended)
  */
-
 contract IntegrationLlamaVesting is IntegrationBase {
   uint256 internal _vestingStartTime;
 
@@ -21,49 +19,51 @@ contract IntegrationLlamaVesting is IntegrationBase {
 
   function test_VestAndUnlock_LaunchDate() public {
     // Before the cliff
-    uint256 _timestamp = VESTING_START_DATE;
+    uint256 _timestamp = LAUNCH_DATE;
     // The unlocking contract holds the tokens
     _warpAndWithdraw(_timestamp);
-    _assertBalances(0);
-    assertEq(_nextToken.balanceOf(address(_connextVestingWallet)), 0);
+    _assertWalletBalance(6_838_356 ether);
+    _assertOwnerBalance(0 ether);
   }
 
   function test_VestAndUnlock_1SecBeforeCliff() public {
     // 1 secont before the cliff
-    uint256 _timestamp = VESTING_START_DATE + 365 days - 1;
+    uint256 _timestamp = LAUNCH_DATE + 365 days - 1;
     _warpAndWithdraw(_timestamp);
-    _assertBalances(0);
-    assertApproxEqAbs(_nextToken.balanceOf(address(_connextVestingWallet)), 6_239_999 ether, MAX_DELTA);
+    _assertWalletBalance(13_078_355 ether);
+    _assertOwnerBalance(0 ether);
   }
 
   function test_VestAndUnlock_Cliff() public {
     // Just at the cliff date
-    uint256 _timestamp = VESTING_START_DATE + 365 days;
+    uint256 _timestamp = LAUNCH_DATE + 365 days;
     _warpAndWithdraw(_timestamp);
-    _assertBalances(1_920_000 ether);
-    assertApproxEqAbs(
-      _nextToken.balanceOf(address(_connextVestingWallet)), 6_240_000 ether - 1_920_000 ether, MAX_DELTA
-    );
+    _assertWalletBalance(11_158_356 ether);
+    _assertOwnerBalance(1_920_000 ether);
   }
 
   function test_VestAndUnlock_1MonthAfterCliff() public {
     // 1 month after the cliff
-    uint256 _timestamp = VESTING_START_DATE + 365 days + 30 days;
+    uint256 _timestamp = LAUNCH_DATE + 365 days + 365 days / 12;
     _warpAndWithdraw(_timestamp);
-    _assertBalances(3_840_000 ether); // 1_920_000 * 2
-    assertApproxEqAbs(
-      _nextToken.balanceOf(address(_connextVestingWallet)), 6_752_876 ether - 3_840_000 ether, MAX_DELTA
-    );
+    _assertWalletBalance(9_758_356 ether);
+    _assertOwnerBalance(3_840_000 ether);
   }
 
   function test_VestAndUnlock_1YearAfterCliff() public {
-    // After the unlocking period has ended
-    uint256 _timestamp = VESTING_START_DATE + 365 days + 365 days;
+    // 1 year after the cliff
+    uint256 _timestamp = LAUNCH_DATE + 365 days + 365 days;
     _warpAndWithdraw(_timestamp);
-    _assertBalances(12_480_000 ether); // half of the total
-    assertApproxEqAbs(
-      _nextToken.balanceOf(address(_connextVestingWallet)), 24_960_000 ether - 12_480_000 ether, MAX_DELTA
-    );
+    _assertWalletBalance(0 ether);
+    _assertOwnerBalance(19_318_356 ether);
+  }
+
+  function test_VestAndUnlock_2YearsAfterCliff() public {
+    // 2 years after the cliff
+    uint256 _timestamp = LAUNCH_DATE + 365 days + 365 days + 365 days;
+    _warpAndWithdraw(_timestamp);
+    _assertWalletBalance(0 ether);
+    _assertOwnerBalance(24_960_000 ether);
   }
 
   /**
@@ -79,8 +79,15 @@ contract IntegrationLlamaVesting is IntegrationBase {
   /**
    * @notice Each withdrawal should equally increase the withdrawn amount and the owner's balance
    */
-  function _assertBalances(uint256 _balance) internal {
+  function _assertOwnerBalance(uint256 _balance) internal {
     assertApproxEqAbs(_connextVestingWallet.released(NEXT_TOKEN_ADDRESS), _balance, MAX_DELTA);
     assertApproxEqAbs(_nextToken.balanceOf(owner), _balance, MAX_DELTA);
+  }
+
+  /**
+   * @notice Assert the connext vesting wallet balance is equal to the given amount (with a delta of MAX_DELTA)
+   */
+  function _assertWalletBalance(uint256 _balance) internal {
+    assertApproxEqAbs(_nextToken.balanceOf(address(_connextVestingWallet)), _balance, MAX_DELTA);
   }
 }
