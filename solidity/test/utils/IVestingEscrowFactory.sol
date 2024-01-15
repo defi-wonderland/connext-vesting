287b6b79be36e7a59f9ae6b87bc36d328bc22e46
Dristpunk <dristpunk@gmail.com>
2024-01-15 17:47:59 +0300

    fix: update interfaces

--- solidity/test/utils/IVestingEscrowFactory.sol
+++ solidity/test/utils/IVestingEscrowFactory.sol
@@ -2,6 +2,32 @@
 pragma solidity 0.8.20;
 
 interface IVestingEscrowFactory {
+  event VestingEscrowCreated(
+    address indexed _funder,
+    address indexed _token,
+    address indexed _recipient,
+    address _escrow,
+    uint256 _amount,
+    uint256 _vestingStart,
+    uint256 _vestingDuration,
+    uint256 _cliffLength
+  );
+
+  function deploy_vesting_contract(
+    address _token,
+    address _recipient,
+    uint256 _amount,
+    uint256 _vestingDuration
+  ) external returns (address _vestingContract);
+
+  function deploy_vesting_contract(
+    address _token,
+    address _recipient,
+    uint256 _amount,
+    uint256 _vestingDuration,
+    uint256 _vestingStart
+  ) external returns (address _vestingContract);
+
   function deploy_vesting_contract(
     address _token,
     address _recipient,
@@ -9,5 +35,9 @@ interface IVestingEscrowFactory {
     uint256 _vestingDuration,
     uint256 _vestingStart,
     uint256 _cliffLength
-  ) external returns (address _vestingEscrowSimple);
+  ) external returns (address _vestingContract);
+
+  function escrows(uint256 _id) external view returns (address _escrow);
+  function escrows_length() external view returns (uint256 _id);
+  function target() external view returns (address _vestingEscrowSimpleTemplate);
 }
