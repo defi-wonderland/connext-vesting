829319dc7aa91c616e15e362e94caa8528f18f7f
Dristpunk <dristpunk@gmail.com>
2024-02-20 14:51:18 +0300

    style: lint and natspec

--- solidity/interfaces/IConnextVestingWallet.sol
+++ solidity/interfaces/IConnextVestingWallet.sol
@@ -25,35 +25,41 @@ interface IConnextVestingWallet {
   /**
    * @notice NEXT token address
    * @dev Mainnet address
+   * @return _nextToken The address of the NEXT token
    */
-  function NEXT_TOKEN() external view returns (address);
+  function NEXT_TOKEN() external view returns (address _nextToken);
 
   /**
    * @notice Token launch date
    * @dev Equals to Sept 5th 2023
+   * @return _timestamp The timestamp of the token launch
    */
-  function NEXT_TOKEN_LAUNCH() external view returns (uint64);
+  function NEXT_TOKEN_LAUNCH() external view returns (uint64 _timestamp);
 
   /**
    * @notice 1 month in seconds (on average)
+   * @return _timedelta The timedelta of one month
    */
-  function ONE_MONTH() external view returns (uint64);
+  function ONE_MONTH() external view returns (uint64 _timedelta);
 
   /**
    * @notice 1 year in seconds
+   * @return _timedelta The timedelta of one year
    */
-  function ONE_YEAR() external view returns (uint64);
+  function ONE_YEAR() external view returns (uint64 _timedelta);
 
   /**
    * @notice Sept 5th 2023 in seconds
+   * @return _timedelta The timestamp of Sept 5th 2023
    */
-  function SEPT_05_2023() external view returns (uint64);
+  function SEPT_05_2023() external view returns (uint64 _timedelta);
 
   /**
    * @notice Vesting cliff duration
    * @dev 1 month cliff
+   * @return _timedelta The timedelta of the cliff duration
    */
-  function VESTING_CLIFF_DURATION() external view returns (uint64);
+  function VESTING_CLIFF_DURATION() external view returns (uint64 _timedelta);
 
   /**
    * NOTE:  The equivalent vesting schedule has a 13 months duration, with a 1 month cliff,
@@ -65,20 +71,23 @@ interface IConnextVestingWallet {
   /**
    * @notice Vesting duration including one month of cliff
    * @dev 13 months duration
+   * @return _timedelta The timedelta of the vesting duration
    */
-  function VESTING_DURATION() external view returns (uint64);
+  function VESTING_DURATION() external view returns (uint64 _timedelta);
 
   /**
    * @notice Vesting warmup time
    * @dev 11 months offset
+   * @return _timedelta The timedelta of the warmup time
    */
-  function VESTING_OFFSET() external view returns (uint64);
+  function VESTING_OFFSET() external view returns (uint64 _timedelta);
 
   /**
    * @notice Vesting start date
    * @dev Sept 5th 2024 - 1 month
+   * @return _timestamp The timestamp of the start date
    */
-  function VESTING_START_DATE() external view returns (uint64);
+  function VESTING_START_DATE() external view returns (uint64 _timestamp);
 
   /*///////////////////////////////////////////////////////////////
                              STORAGE
@@ -87,14 +96,16 @@ interface IConnextVestingWallet {
   /**
    * @notice Total amount of tokens to be vested
    * @dev Set into constructor
+   * @return _amount The total amount of tokens
    */
-  function TOTAL_AMOUNT() external view returns (uint256);
+  function TOTAL_AMOUNT() external view returns (uint256 _amount);
 
   /**
    * @notice The cliff timestamp
    * @dev Set into constructor
+   * @return _timestamp The timestamp of the cliff
    */
-  function CLIFF() external view returns (uint64);
+  function CLIFF() external view returns (uint64 _timestamp);
 
   /*///////////////////////////////////////////////////////////////
                             CUSTOM LOGIC
